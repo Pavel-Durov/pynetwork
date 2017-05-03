9ac0a124de0bfe4a6b6b5ab2d9b83a9912d1e969
Pavel-Durov <pdurov0@gmail.com>
2017-05-03 16:09:02 +0300

    setup.py refactoring

--- setup.py
+++ setup.py
@@ -5,23 +5,14 @@ README = open('README.md').read()
 
 setup(
     name="pynetwork",
-    version="1.0.0",
+    version="0.1dev",
+    packages=['pynetwork'],
     license='MIT',
     description='Network ping/upload/download speed measurements analysis and reports',
     long_description=README,
     url='https://github.com/Pavel-Durov/pynetwork',
-    author='Pavel Durov',
-    author_email='pdurov0@gmail.com',
-    py_modules=["pynetwork"],
     install_requires=[
         'mail', 'requests', 'google-api-python-client', 'httplib2', 'jinja2', 'slackclient'
     ],
-    classifiers=[
-        'Programming Language: Python 2.7',
-        'Programming Language: Python 3.*'
-    ],
-    entry_points={
-        'console_scripts': ['pynetwork=pynetwork:main']
-    },
     keywords=['network', 'speedtest', 'notifications', 'reports', 'speed', 'test', 'bandwidth']
 )
